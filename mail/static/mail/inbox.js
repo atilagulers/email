bc28ab53a185e4801f655320e7e313ff50be2411
Atila Guler <atilagulerdev@gmail.com>
2024-02-07 01:29:36 +0300

    finish project

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -43,7 +43,18 @@ async function send_email(e) {
   });
 
   const data = await response.json();
+  const message_box = document.querySelector('#message-box');
+
+  show_message(data.message, data.error ? 'error' : 'success');
+
   load_mailbox('sent');
+}
+
+function show_message(message, type) {
+  const message_box = document.querySelector('#message-box');
+  message_box.innerHTML = message;
+  message_box.style.display = 'block';
+  message_box.classList.add(`${type}`);
 }
 
 function compose_email() {
@@ -86,46 +97,101 @@ async function load_mailbox(mailbox) {
     email_element.addEventListener('click', () => view_email(email.id));
     emails_view.appendChild(email_element);
   });
+}
 
-  async function view_email(id) {
-    hide_all_views();
-    document.querySelector('#email-view').style.display = 'block';
-    const email_view = document.querySelector('#email-view');
+async function view_email(id) {
+  hide_all_views();
+  document.querySelector('#email-view').style.display = 'block';
+  const email_view = document.querySelector('#email-view');
+  email_view.innerHTML = '';
 
-    const email_element = document.createElement('div');
+  const response = await fetch(`/emails/${id}`);
+  const email = await response.json();
+  await read_email(id);
 
-    const response = await fetch(`/emails/${id}`);
-    const email = await response.json();
-    await read_email(id);
-
-    email_element.innerHTML = `
-    <p id=''><strong>From:</strong> ${email.sender}</p>
-    <p id=''><strong>To:</strong> ${email.recipients}</p>
-    <p id=''><strong>Subject:</strong> ${email.subject}</p>
-    <p id=''><strong>Timestamp:</strong>${email.timestamp}</p>
-    <button id='archive'>Archive</button>
+  email_view.innerHTML = `
+    <p id=''><strong>From: </strong>${email.sender}</p>
+    <p id=''><strong>To: </strong>${email.recipients}</p>
+    <p id=''><strong>Subject: </strong>${email.subject}</p>
+    <p id=''><strong>Timestamp: </strong>${email.timestamp}</p>
     `;
 
-    email_element.innerHTML += `<button id='reply'>Reply</button>`;
-    email_element.innerHTML += `<p id='email-body'>${email.body}</p>`;
-    email_view.appendChild(email_element);
-  }
+  // Reply button
+  const reply_button = document.createElement('button');
+  reply_button.classList.add('btn', 'btn-primary');
+  reply_button.style.marginRight = '10px';
+  reply_button.innerHTML = 'Reply';
+  reply_button.addEventListener('click', () => {
+    compose_email();
+    document.querySelector('#compose-recipients').value = email.sender;
+    document.querySelector('#compose-subject').value = `Re: ${email.subject}`;
+    document.querySelector(
+      '#compose-body'
+    ).value = `On ${email.timestamp} ${email.sender} wrote: ${email.body}\n`;
 
-  async function read_email(id) {
-    console.log(id);
-    const response = await fetch(`/emails/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        read: true,
-      }),
-    });
+    document.querySelector('#compose-body').focus();
+  });
+  email_view.appendChild(reply_button);
 
-    console.log(response);
-  }
+  // Archive button
+  const archive_button = document.createElement('button');
+  archive_button.classList.add('btn');
+  archive_button.classList.add(
+    email.archived ? 'btn-outline-primary' : 'btn-outline-danger'
+  );
+  archive_button.innerHTML = email.archived ? 'Unarchive' : 'Archive';
+  archive_button.addEventListener('click', async () => {
+    console.log(email.id, email.archived);
+    await update_archive(email.id, !email.archived);
 
-  async function fetch_emails(mailbox) {
-    const response = await fetch(`/emails/${mailbox}`);
-    const data = await response.json();
-    return data;
-  }
+    if (email.archived) {
+      load_mailbox('inbox');
+    } else {
+      load_mailbox('archive');
+    }
+  });
+
+  email_view.appendChild(archive_button);
+
+  // Email body
+  const email_body = document.createElement('p');
+  email_body.id = 'email-body';
+  email_body.textContent = email.body;
+
+  // Add hr
+  const hr = document.createElement('hr');
+  email_view.appendChild(hr);
+
+  // Append to email view
+  email_view.appendChild(email_body);
+}
+
+async function read_email(id) {
+  const response = await fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: true,
+    }),
+  });
+}
+
+async function update_archive(id, archived) {
+  const response = await fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived,
+    }),
+  });
+
+  const msg = archived
+    ? 'Email archived successfully.'
+    : 'Email unarchived successfully.';
+
+  show_message(msg, 'success');
+}
+
+async function fetch_emails(mailbox) {
+  const response = await fetch(`/emails/${mailbox}`);
+  const data = await response.json();
+  return data;
 }
